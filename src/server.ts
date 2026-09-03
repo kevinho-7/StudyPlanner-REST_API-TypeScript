acd3464e1fef358f543de0d39c2aef19de01949e
kevinho-7 <kevingarciaf.kf@outlook.com>
2026-09-02 21:22:59 -0300

    written server.ts in typescript way

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import Express = require("express");
+import express, { type Express, type Request, type Response } from 'express';
 
-const app = Express();
+const app: Express = express();
 
-app.get("/", (req, res) => {
-    res.send("Heyy whats up??\n Welcome to 'Study Planner API!'")    
+app.get("/", (req: Request, res: Response) => {
+    res.send("Heyy whats up??\n Welcome to 'Study Planner API!'");
 });
 
-app.listen(5000, () => {
+app.listen(5050, () => {
     console.log("Server runing on port 5000")
 });
+
